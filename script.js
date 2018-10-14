6d7cd00801bf86788c12f4470c3499708af8344f
Filip Lipowiecki <lipowiecki.f@gmail.com>
2018-10-14 15:14:20 +0200

    imageLoading

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function appStart() {
-    pojemnik 
+document.addEventListener("DOMContentLoaded", start);
+console.log('działa');
+let canvas;
+let ctx;
+let image;
+function start(){
+    console.log('działa');
+    loadImg();
+}
+function loadImg (){
+    canvas = document.getElementById("editor");
+    ctx = canvas.getContext("2d");
+    console.log('dziala');
+    image = new Image();
+    image.src = 'https://www.wykop.pl/cdn/c3201142/comment_MzEwETvgHANGlzTFlFo9SA22cXLFiOAJ.jpg';
+    image.onload = function() {
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    }
 }
