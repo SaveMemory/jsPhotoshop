0ef08b1be35a5d1048950a030938d23c85daae5a
Lipowiecki Filip <filip.lipowiecki@microsoft.wsei.edu.pl>
2018-10-16 17:50:46 +0100

    blurFilterStarted

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ console.log('działa');
 let canvas = document.getElementById("editor");
 let ctx = canvas.getContext("2d");
 let image;
-let  mouseDown = false;
+let mouseDown = false;
+let blurBtn = document.querySelector("blurBtn");
+let imgData = con.getImageData(0, 0, canvas.width, canvas.height);
 
 function start(){
     console.log('działa');
@@ -17,6 +19,7 @@ function start(){
         let mousePosition = getMousePosition(canvas, event);
         let positionX = mousePosition.x;
         let positionY = mousePosition.y;
+        console.log(event.screenX)
         draw(canvas, positionX, positionY);
     });
 }
@@ -44,8 +47,25 @@ function getMousePosition(canvas, event){
         y:event.clientY - rectangle.top}
 }
 
-function draw (canvas, positionX, positionY){
+function draw (positionX, positionY){
     if(mouseDown){
         ctx.fillRect(positionX, positionY, 4, 4);
+    }
+}
+
+function blurImg (w, h){
+    for(let i=0; i<h; i++)
+    {
+        for(let j=0; j<w; j++)
+        {
+            index = (w + (h * imgData.width)) * 4;
+            //separate into color values
+            r = imgData.data[index];
+            g = imgData.data[index + 1];
+            b = imgData.data[index + 2];
+            a = imgData.data[index + 3];
+            let blurred = r + g + b + a;
+            r
+        }
     }
 }
